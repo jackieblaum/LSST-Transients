51d8ed976281a131ed3f5eb167640e4861277b66
Jackie Blaum <jackie.blaum@gmail.com>
2017-07-07 16:17:57 -0500

    Pixels to WCS

---
+++ lsst_transients/utils/pix2world.py
@@ -0,0 +1,26 @@
+import numpy
+from astropy import wcs
+from astropy.io import fits as pyfits
+
+def pix2world(filename, array):
+    '''
+    Converts the pixels of the array into the WCS, which is read from the given file.
+
+    :param filename: The file to be read
+    :param array: The array to be converted from pixels to WCS
+    :return: The converted array (in WCS)
+    '''
+
+    # Load the FITS hdulist
+    hdulist = pyfits.open(filename)
+
+    # Parse the WCS keywords in the primary HDU
+    w = wcs.WCS(hdulist[0].header)
+
+    # Pixel coordinates
+    pixels = numpy.array(array, numpy.float)
+
+    # Convert the pixels to WCS
+    world = w.wcs_pix2world(pixels, 0)
+
+    return world
