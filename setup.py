9d420443aa6d15e0254401e46abdb4da0d90facb
Travis CI <travis@travis-ci.org>
2017-07-20 16:10:49 -0700

    Initial setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='LSST-Transients',
+    version='',
+    packages=['lsst_transients', 'lsst_transients.utils'],
+    url='',
+    license='',
+    author='Jackie Blaum, Giacomo Vianello',
+    author_email='',
+    description='',
+    install_requires=['numpy',
+                      'scipy',
+                      'astropy'],
+)
