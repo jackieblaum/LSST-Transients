9a87abdea987a8596678e6410fbb7a2aee4a12b1
Jackie Blaum <jackie.blaum@gmail.com>
2017-07-21 13:28:04 -0700

    Minor fixes

--- scripts/lsst_get_data.py
+++ scripts/lsst_get_data.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
         
     db = Data_Database("lsst_transients.db")
-    db.fill_visits(args.regfile, args.path)
+    db.fill_visits(args.path)
     db.close()
