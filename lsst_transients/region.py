c3cdbcc548050df52d31a02a853a6c6fd0876a14
Jackie Blaum <jackie.blaum@gmail.com>
2017-07-24 11:13:47 -0700

    Minor changes for Sqlite

--- lsst_transients/region.py
+++ lsst_transients/region.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Region(object):
     '''
     A Region object is a square or circular region, many of which form a grid.
@@ -46,4 +48,42 @@ class Region(object):
 
         elif self._shape == "circle":
             return "circle(" + str(self.x) + ", " + str(self._y) + ", " + str(self._d) +")"
+
+
+    def get_boundingbox(self, maxx, maxy):
+        '''
+
+        :return corners: An array of sky coordinate pairs for the four corners of the bounding box with a padding of 5 pixels
+        '''
+
+        padding = 5
+
+        corner1 = [int(np.floor(float(self.x) - (float(self.d) / 2.0) - padding)),
+                   int(np.floor(float(self.y) - (float(self.d) / 2.0) - padding))]
+        corner2 = [int(np.floor(float(self.x) - (float(self.d) / 2.0) - padding)),
+                   int(np.ceil(float(self.y) + (float(self.d) / 2.0) + padding))]
+        corner3 = [int(np.ceil(float(self.x) + (float(self.d) / 2.0) + padding)),
+                   int(np.floor(float(self.y) - (float(self.d) / 2.0) - padding))]
+        corner4 = [int(np.ceil(float(self.x) + (float(self.d) / 2.0) + padding)),
+                   int(np.ceil(float(self.y) + (float(self.d) / 2.0) + padding))]
+
+        if corner1[0] < 1:
+            corner1[0] = 1
+        if corner1[1] < 1:
+            corner1[1] = 1
+        if corner2[0] < 1:
+            corner2[0] = 1
+        if corner2[1] > int(maxy):
+            corner2[1] = int(maxy)
+        if corner3[0] > int(maxx):
+            corner3[0] = int(maxx)
+        if corner3[1] < 1:
+            corner3[1] = 1
+        if corner4[0] > int(maxx):
+            corner4[0] = int(maxx)
+        if corner4[1] > int(maxy):
+            corner4[1] = int(maxy)
+
+        return [corner1, corner2, corner3, corner4]
+
 
