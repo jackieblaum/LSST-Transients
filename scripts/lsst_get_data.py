2217335e9618bd6747501b2ea641c11581eb38a7
Jackie Blaum <jackie.blaum@gmail.com>
2017-07-25 13:21:22 -0700

    Options for whether or not to write the flux and conditions tables

--- scripts/lsst_get_data.py
+++ scripts/lsst_get_data.py
@@ -8,9 +8,12 @@ if __name__ == "__main__":
     # Collect input from the user
     parser = argparse.ArgumentParser(description="Collect information from each visit")
     parser.add_argument('-p', '--path', type=str, help='Input path to folder with all visit files', required=True)
+    parser.add_argument('-n', '--name', type=str, help='Name for the database', required=True)
+    parser.add_argument('-f', '--flux', type=str, help='Get the fluxes from the files (True or False)', required=True)
+    parser.add_argument('-c', '--conditions', type=str, help='Get the conditions from the files (True or False)', required=True)
     
     args = parser.parse_args()
         
-    db = Data_Database("lsst_transients.db")
-    db.fill_visits(args.path)
+    db = Data_Database("%s.db" % args.name)
+    db.fill_visits(args.path, args.flux, args.conditions)
     db.close()
