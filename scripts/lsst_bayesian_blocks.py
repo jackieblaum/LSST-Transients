c793851e59eedef3d4de7e95debd36346a8ffca7
Jackie Blaum <jackie.blaum@gmail.com>
2017-07-25 15:16:48 -0700

    New script

---
+++ scripts/lsst_bayesian_blocks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+
+from lsst_transients.run_bayesian_blocks import BayesianBlocks
+
+if __name__ == "__main__":
+
+    # Collect input from the user
+    parser = argparse.ArgumentParser(description="Collect information from each visit")
+    parser.add_argument('-s', '--sort', type=str, help='Sort the visits in the database (True or False)', required=True)
+    parser.add_argument('-n', '--name', type=str, help='Name of the database (without .db)', required=True)
+
+    args = parser.parse_args()
+
+    bb = BayesianBlocks(args.name)
+    bb.run_algorithm(args.sort)
+
+
