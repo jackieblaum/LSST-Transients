f83a3934fa4816511a45155a9d9e43e1ef65786d
Jackie Blaum <jackie.blaum@gmail.com>
2017-08-02 23:03:14 -0700

    Switched to new database format

--- lsst_transients/examine_transient_candidates.py
+++ lsst_transients/examine_transient_candidates.py
@@ -1,5 +1,6 @@
 import yaml
 import re
+import os
 import matplotlib.pyplot as plt
 
 
@@ -22,7 +23,7 @@ def get_regs_and_blk_edges(yaml_file):
     return reg_ids, block_edges
 
 
-def write_ds9_region_files(reg_ids, db):
+def write_ds9_region_files(reg_ids, db, directory):
     '''
     Writes all transient candidate regions to their own DS9 region files.
 
@@ -36,17 +37,19 @@ def write_ds9_region_files(reg_ids, db):
     # Loop over the regions that contain transient candidates
     for i, region in enumerate(reg_ids):
 
-        # Region indices start at 0, so subtract one from the region ID
-        reg_index = int(re.compile(r'(\d+)$').search(region).group(1)) - 1
+        reg_index = int(re.compile(r'(\d+)$').search(region).group(1))+1
 
         # Get the DS9 region string from the database and write it to a new region file
-        with open('%s.reg' % region, "w+") as f:
+        with open('%s/%s.reg' % (directory, region), "w+") as f:
 
             ds9_string = df['ds9_info'][reg_index]
 
             f.write('icrs\n%s' % ds9_string)
 
-def make_lightcurves(reg_ids, block_edges, db):
+def make_lightcurves(reg_ids, block_edges, db, directory):
+
+    df_fluxes = db.db.get_table_as_dataframe('fluxes')
+    df_errors = db.db.get_table_as_dataframe('fluxes_errors')
 
     # Loop over the transient candidate regions
     for i in range(len(reg_ids)):
@@ -54,7 +57,6 @@ def make_lightcurves(reg_ids, block_edges, db):
         reg_database_index = re.compile(r'(\d+)$').search(reg_ids[i]).group(1)
 
         # Get the dataframe that corresponds with the region
-        df = db.db.get_table_as_dataframe('flux_table_%s' % reg_database_index)
         cond = db.db.get_table_as_dataframe('cond_table')
         times = cond['date (modified Julian)']
 
@@ -63,17 +65,17 @@ def make_lightcurves(reg_ids, block_edges, db):
         plt.xlabel('Time (MJD)')
         plt.ylabel('Flux')
 
-        plt.errorbar(times, df['flux'].values, yerr=list(df['err'].values), fmt='.')
-        plt.title('Region %i Lightcurve' % i)
+        plt.errorbar(times, df_fluxes.iloc[int(reg_database_index)].values,
+                     yerr=list(df_errors.iloc[int(reg_database_index)].values), fmt='.')
+        plt.title('Region %s Lightcurve' % reg_database_index)
 
-        plt.yscale("symlog")
+        #plt.yscale("symlog")
 
         for j in range(len(block_edges[i])):
-            edge_lines = plt.axvline(block_edges[i].item(j), linestyle='--')
+            edge_lines = plt.axvline(block_edges[i][j], linestyle='--')
             plt.setp(edge_lines, color='r', linewidth=2.0)
 
-        plt.savefig("%s.png" % reg_ids[i])
-        plt.show()
+        plt.savefig("%s/%s.png" % (directory, reg_ids[i]))
 
 
 
@@ -84,9 +86,14 @@ def examine_transient_candidates(database, block_edges_file):
     # Get the region IDs and corresponding block edges of transient candidates from the block edges file
     reg_ids, block_edges = get_regs_and_blk_edges(block_edges_file)
 
-    # Make a new DS9 region file for each individual transient candidate region
-    write_ds9_region_files(reg_ids, db)
+    # Make a directory to store the region files and lightcurves
+    directory = 'transient_candidates'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    make_lightcurves(reg_ids, block_edges, db)
+    # Make a new DS9 region file for each individual transient candidate region
+    write_ds9_region_files(reg_ids, db, directory)
+
+    make_lightcurves(reg_ids, block_edges, db, directory)
 
     db.close()
